6744e5379ea07e69c1489fb7cb7e9b9aa481e35d
polarius <tarasov.slavas2002@gmail.com>
2026-01-18 11:39:28 +0300

    Update API URL to production backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -130,6 +130,6 @@ class Api {
   }
 }
 // Замените на адрес вашего бэкенда
-const api = new Api("http://localhost:3000");
+const api = new Api("https://api.svyatsite.nomoreparties.co");
 
 export default api;
